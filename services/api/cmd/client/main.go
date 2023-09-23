328942121b0aced42b57d6e94af382c12e2481f7
albugowy15 <kholidbughowi@gmail.com>
2023-09-23 12:47:18 +0700

    feat: initialize api service

---
+++ services/api/cmd/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	host = "localhost"
+	port = "5000"
+)
+
+func main() {
+	addr := fmt.Sprintf("%s:%s", host, port)
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Println("could not connect to grpc server: ", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+}
---
+++ services/api/cmd/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	host = "localhost"
+	port = "5000"
+)
+
+func main() {
+	addr := fmt.Sprintf("%s:%s", host, port)
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Println("error starting tcp listener: ", err)
+		os.Exit(1)
+	}
+	log.Println("tcp listener started at port: ", port)
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Println("error serving grpc: ", err)
+		os.Exit(1)
+	}
+}
