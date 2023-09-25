cc0192f0e67cc737f594fa8953f8621effe28779
albugowy15 <kholidbughowi@gmail.com>
2023-09-26 03:50:26 +0700

    feat: grpc server

--- services/api/cmd/server/main.go
+++ services/api/cmd/server/main.go
@@ -1,32 +1,41 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net"
-	"os"
+
+	pb "api/proto/greater"
 
 	"google.golang.org/grpc"
 )
 
 var (
-	host = "localhost"
-	port = "5000"
+	port = flag.Int("port", 50051, "The server port")
 )
 
+type Server struct {
+	pb.UnimplementedGreaterServer
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
 func main() {
-	addr := fmt.Sprintf("%s:%s", host, port)
-	lis, err := net.Listen("tcp", addr)
-
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		log.Println("error starting tcp listener: ", err)
-		os.Exit(1)
+		log.Fatalf("failed to listen: %v", err)
 	}
-	log.Println("tcp listener started at port: ", port)
-	grpcServer := grpc.NewServer()
 
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Println("error serving grpc: ", err)
-		os.Exit(1)
+	s := grpc.NewServer()
+	pb.RegisterGreaterServer(s, &Server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
